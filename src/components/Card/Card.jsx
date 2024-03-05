12115af22d708b74825f641942a381b4a15cd1c5
SuhasiV <130243610+SuhasiV@users.noreply.github.com>
2024-03-05 14:04:57 +0530

    Update Card.jsx - removed process.env extension

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,7 +10,6 @@ const Card = ({ item }) => {
           {item?.attributes.isNew && <span>New Season</span>}
           <img
             src={
-              process.env.REACT_APP_UPLOAD_URL +
               item.attributes?.img?.data?.attributes?.url
             }
             alt=""
@@ -18,7 +17,6 @@ const Card = ({ item }) => {
           />
           <img
             src={
-              process.env.REACT_APP_UPLOAD_URL +
               item.attributes?.img2?.data?.attributes?.url
             }
             alt=""
